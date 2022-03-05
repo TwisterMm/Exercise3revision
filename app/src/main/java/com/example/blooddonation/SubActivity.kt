0ce1c3a9dc45e5e8422ac3daf515600972e08d88
TwisterMm <wesleytee@gmail.com>
2022-03-05 22:00:01 +0800

    complete the subactivity

--- app/src/main/java/com/example/blooddonation/SubActivity.kt
+++ app/src/main/java/com/example/blooddonation/SubActivity.kt
@@ -12,5 +12,32 @@ class SubActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener { finish() }
+
+        val weight    = intent.getDoubleExtra("weight", 0.0)
+        val height    = intent.getDoubleExtra("height", 0.0)
+        val bmi       = intent.getDoubleExtra("bmi", 0.0)
+        val status    = intent.getStringExtra("status") ?: ""
+        val condition = intent.getStringExtra("condition") ?: ""
+        val firstTime = intent.getBooleanExtra("firstTime", false)
+        val age       = intent.getIntExtra("age", 0)
+        val eligible  = intent.getBooleanExtra("eligible", false)
+
+        with(binding){
+            outtWeight.text = "%.2f".format(weight)
+            outHeight.text = "%.2f".format(height)
+            edtBMI.text = "%.2f".format(bmi)
+            outAge.text = age.toString()
+            edtStatus.text = status
+            outCon.text = condition
+            edtFTD.text = if(firstTime) "Yes" else "No"
+
+            if(eligible){
+                outEligible.text = "Eligible"
+                outEligible.setTextColor(getColor(R.color.teal_200))
+            }else{
+                outEligible.text = "Not Eligible"
+                outEligible.setTextColor(getColor(R.color.red))
+            }
+        }
     }
 }
